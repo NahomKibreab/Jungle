e5114815d43623a3f6f181514fa7ba5acde18696
Nahom Mehanzel <nknahom@gmail.com>
2021-09-21 04:35:38 +0000

    Add password validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    context 'password and password_confirmation' do
+      it ('should return true if both field are equal') do
+        @user = User.new({first_name:'Nahom', last_name: 'kibreab', email:'email@gmail.com', password:'password', password_confirmation: 'password'})
+        expect(@user.save).to be true
+      end
+
+      it ('should return false if both field are equal') do
+        @user = User.new({first_name:'Nahom', last_name: 'kibreab', email:'email@gmail.com', password:'password1', password_confirmation: 'password'})
+        expect(@user.save).to be false
+      end
+
+      it ('should return false if ethier password is missed') do
+        @user = User.new({first_name:'Nahom', last_name: 'kibreab', email:'email@gmail.com', password_confirmation: 'password'})
+        expect(@user.save).to be false
+      end
+    end
+  end
 end
